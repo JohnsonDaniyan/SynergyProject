b2cdd6e4f254d399f2eb65784fbbe693e9b8ace4
JohnsonDaniyan <toyinjohnsondaniyan.td@gmail.com>
2022-10-01 20:27:44 +0100

    update

--- src/components/Navbar2.jsx
+++ src/components/Navbar2.jsx
@@ -26,7 +26,7 @@ function Navbar2() {
        </Example>
   </div>
   <div class="hidden justify-between items-center w-full absolute left-1/2 -translate-x-1/2 md:flex md:w-auto md:order-1" id="navbar-sticky">
-  <ul class="tracking-widest flex flex-col p-4 mt-4 bg-black rounded-lg border border-gray-100 md:flex-row md:space-x-3 lg:space-x-8 md:mt-0 md:text-lg lg:text-3xl md:font-medium md:border-0 md:bg-black dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700 w-1000 justify-between">
+  <ul class="tracking-widest flex flex-col p-4 mt-4 bg-black rounded-lg border border-gray-100 md:flex-row md:space-x-3 lg:space-x-8 md:mt-0 md:text-lg lg:text-3xl md:font-medium md:border-0 md:bg-black w-1000 justify-between">
     <li>
       <Link to="/farm" class="block py-2 pr-4 pl-3 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">FARM</Link>
     </li>
